15f7e07dabf6684a7a93a056c595fb0e66e6aa98
gtoth <>
2009-06-23 15:45:36 +0000

    Modified script so that the test is running on a different machine than the webserver.

--- Scripts/test_crash.sh
+++ Scripts/test_crash.sh
@@ -1,24 +1,28 @@
 #!/bin/csh
-cd ${HOME}/Sites
-# remove old test
+cd ${HOME}/CRASHTEST
+
+# remove old test and move last test to old one
 rm -rf CRASH_OLD
-# store current test as old
 mv CRASH CRASH_OLD
+
+# do this on herot too for the webpage
+ssh herot "cd Sites; rm -rf CRASH_OLD; mv CRASH CRASH_OLD"
+
 # checkout BATSRUS code and rename it as CRASH
 cvs co BATSRUS
 mv BATSRUS CRASH
-# do not allow people to look at the whole code
-chmod -R go-r CRASH
+
 # Install CRASH
 cd CRASH
 Config.pl -install -mpi=openmpi
+
 # Checkout the CRASHTEST repository
 cvs co CRASHTEST
-# Allow people to look at CRASH/CRASHTEST while test is running
-cd CRASHTEST
-chmod -R go+r .
+
 # Execute all tests. The < /dev/null is needed so that IDL runs even if the
 # user is not logged in.
-make test MPIRUN='mpirun -np 6' >& test.log < /dev/null
-# Allow people to look at final results
-chmod -R go+r .
+cd CRASHTEST
+make test MPIRUN='mpirun -np 16' >& test.log < /dev/null
+
+# Copy results to a web site
+rsync -az --delete ~/CRASHTEST/CRASH/CRASHTEST/ herot:Sites/CRASH/CRASHTEST/
