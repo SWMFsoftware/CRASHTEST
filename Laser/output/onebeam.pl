318ccffbf567299dc34381c0c84b6aec7e7b3664
bartvand <>
2011-08-18 15:47:30 +0000

    It turns out that the ray test failed because the effective collision rate was not set proportional to the density. Modified Files: 	CRASHTEST/Laser/Makefile CRASHTEST/Laser/output/onebeam.pl 	src/ModLaserHeating.f90 Removed Files: 	CRASHTEST/Laser/output/onebeam_amr.pl

--- Laser/output/onebeam.pl
+++ Laser/output/onebeam.pl
@@ -8,24 +8,24 @@ my @TurningPoint;
 my @SumLaserHeating;
 my @SumLaserHeatingRef;
 my $i;
+my $Test = $ARGV[0];
 
 foreach $i (0, 1, 2)
 {
     $RES = 2**$i;
 
-    $NameFile = "test_onebeam_results/".$RES."/runlog" || die("Cannot Open File");
-
+    $NameFile = $Test."_results/".$RES."/runlog" || die("Cannot Open File");
     open(FILE, $NameFile);
     while(<FILE>){
 	@PosXhold[$i]=$1           if /\bmaximum X position of ray\s*=\s*([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)/i;
 	@TurningPoint[$i]=$1       if /\bAnalytical turning point\s*=\s*([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)/i;
 	@SumLaserHeating[$i]=$1    if /\bsum laser heating\s*=\s*([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)/i;
-	@SumLaserHeatingRef[$i]=$1 if /\bAnalytical factional absorption\s*=\s*([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)/i;
+	@SumLaserHeatingRef[$i]=$1 if /\bAnalytical fractional absorption\s*=\s*([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)/i;
     }
     close FILE;
 }
 
-$NameFile = ">test_onebeam_results/error.dat";
+$NameFile = ">".$Test."_results/error.dat";
 
 open(FILE, $NameFile);
 print FILE "resolution turning_point_error energy_deposition_error\n";
