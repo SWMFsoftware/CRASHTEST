94018c59bfb94bf4b113635fe54b77f23ba19082
bartvand <>
2012-02-12 01:36:52 +0000

    improved CRASHTEST script

--- Scripts/test_crash.sh
+++ Scripts/test_crash.sh
@@ -5,9 +5,6 @@ cd ${HOME}/CRASHTEST
 rm -rf CRASH_OLD
 mv CRASH CRASH_OLD
 
-# do this on herot too for the webpage
-ssh herot "cd Sites; rm -rf CRASH_OLD; mv CRASH CRASH_OLD; mkdir CRASH"
-
 # checkout BATSRUS code and rename it as CRASH
 cvs co BATSRUS
 mv BATSRUS CRASH
@@ -20,17 +17,22 @@ cd ..
 # Install CRASH
 Config.pl -install -hypre
 
+# Set current date of check-out
+setenv CHECKOUTDATE `date +%Y.%m.%d`
+
 # Checkout the CRASHTEST repository
 cvs co CRASHTEST
 
 # Execute all tests. The < /dev/null is needed so that IDL runs even if the
 # user is not logged in.
-
 cd CRASHTEST
 make test MPIRUN='mpirun -np 16' >& test.log < /dev/null
 
+# do this on herot too for the webpage
+ssh herot "cd Sites; rm -rf CRASH_OLD; mv CRASH CRASH_OLD; mkdir CRASH"
+
 # Store result by yesterday's date
-scp test_results.txt herot:Sites/CRASH_RESULTS/`date -v-1d +%Y.%m.%d`
+scp test_results.txt herot:Sites/CRASH_RESULTS/${CHECKOUTDATE}
 
 # Copy results to a web site
-rsync -a ~/CRASHTEST/CRASH/CRASHTEST/ herot:Sites/CRASH/CRASHTEST/ < /dev/null
+rsync -az --delete ~/CRASHTEST/CRASH/CRASHTEST/ herot:Sites/CRASH/CRASHTEST/ < /dev/null
